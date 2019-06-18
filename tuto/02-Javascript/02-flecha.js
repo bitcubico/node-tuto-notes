51fe9e8cebffdd1530180731f72bea0352306986
mcubico <mcubico33@gmail.com>
2019-06-18 08:49:19 -0500

    Agrego ejemplos de como usar la funcion de flecha

---
+++ tuto/02-Javascript/02-flecha.js
@@ -0,0 +1,36 @@
+function sumar(a, b) {
+    return a + b;
+}
+
+let sumaFlecha = (a, b) => a + b;
+
+let dividir = (a, b) => {
+    if (b == 0)
+        return 0;
+
+    return a / b;
+};
+
+let dividir_2 = (a, b) => b == 0 ? 0 : a / b;
+
+console.log(sumar(5, 5));
+console.log(sumaFlecha(5, 5));
+console.log(dividir(5, 5));
+console.log(dividir(5, 0));
+console.log(dividir_2(5, 5));
+
+// El apuntador this dentro de una función de flecha apunta a los objetos que están por fuera de la función.
+// Por esta razón se generará una excepción al tratar de obtener los datos del objeto con el this
+let heroe = {
+    name: 'Dead Pool',
+    firstName: 'Wade',
+    lastName: 'Winston',
+    power: 'Regeneración',
+    getFullName: () => {
+        return `Heroe: ${this.name} - Nombre Real: ${this.firstName} ${this.lastName} - Poder: ${this.power}`;
+    }
+};
+
+console.log(heroe.getFullName());
+
+// Para que esto funcione correctamente,
