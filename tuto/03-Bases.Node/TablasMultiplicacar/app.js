a3bb44679c3adbaa08166ef1bb88b5f160bab380
mcubico <mcubico33@gmail.com>
2019-06-19 08:41:29 -0500

    Agrego la posibilidad de recibir la base de multiplicación por parámetro

--- tuto/03-Bases.Node/TablasMultiplicacar/app.js
+++ tuto/03-Bases.Node/TablasMultiplicacar/app.js
@@ -2,7 +2,12 @@ const multiplicar = require('./Core/multiplicar')
     // const {BuildFile} = require('./Core/multiplicar') // Otra forma
 
 // console.log(module);
-let base = 105;
+// console.log(process);
+// console.log(process.argv);
+
+// Para que esto funcione se debe llamar app.js agregando los parámetros de entrada, así:
+// node app --base=<numero_base>
+let base = process.argv[2] != undefined ? process.argv[2].split('=')[1] : 1;
 
 multiplicar.BuildFile(base)
     .then(file => console.log(`El archivo [${file}] fué creado exitosamente.`))
