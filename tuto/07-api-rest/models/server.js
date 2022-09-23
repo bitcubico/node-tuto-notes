c5b0019ca8d9776b55f0834fd1cdc84106a8cb29
Mauricio Montoya Medrano <mauricio.montoya@belltech.la>
2022-09-23 11:30:51 -0500

    Agrego las rutas base y uso el plugin http-status-code para agregarlos a las repuestas de la api

--- tuto/07-api-rest/models/server.js
+++ tuto/07-api-rest/models/server.js
@@ -1,21 +1,71 @@
 const express = require('express');
+const httpStatus = require('http-status-codes');
 
-// Clase usada para inicializar el servidor
+/**
+ * Servidor web con la api
+ *
+ * @class Server
+ */
 class Server {
 
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
 
+    // Middlewares
+    this.middlewares();
+
+    // Routes
     this.routes();
   }
 
-  // Configura las rutas
-  routes() {
-    this.app.get('/', (req, res) => res.send('Hello World'));
+  /**
+   * Procesos a ejecutarse antes de llegar a las rutas
+   *
+   * @memberof Server
+   */
+  middlewares() {
+    this.app.use(express.static('public'));
   }
 
-  // Se usa para inicializar el servidor
+  /**
+   * Rutas configuradas del servidor
+   *
+   * @memberof Server
+   */
+  routes() {
+    this.app.get('/api', (req, res) => res.status(httpStatus.StatusCodes.OK).json({
+      status: httpStatus.ReasonPhrases.OK,
+      message: 'Hello from get',
+    }));
+
+    this.app.post('/api', (req, res) => res.status(httpStatus.StatusCodes.CREATED).json({
+      status: httpStatus.ReasonPhrases.CREATED,
+      message: 'Hello from post',
+    }));
+
+    
+    this.app.put('/api', (req, res) => res.status(httpStatus.StatusCodes.ACCEPTED).json({
+      status: httpStatus.ReasonPhrases.ACCEPTED,
+      message: 'Hello from put',
+    }));
+
+    this.app.patch('/api', (req, res) => res.status(httpStatus.StatusCodes.ACCEPTED).json({
+      status: httpStatus.ReasonPhrases.ACCEPTED,
+      message: 'Hello from patch',
+    }));
+
+    this.app.delete('/api', (req, res) => res.status(httpStatus.StatusCodes.OK).json({
+      status: httpStatus.ReasonPhrases.OK,
+      message: 'Hello from delete',
+    }));
+  }
+
+  /**
+   * Levanta el servidor
+   *
+   * @memberof Server
+   */
   listen() {
     this.app.listen(this.port, () =>
       console.log(`Server running in port ${this.port}`)
